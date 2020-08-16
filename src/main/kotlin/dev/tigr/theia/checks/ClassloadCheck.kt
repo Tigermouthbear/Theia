7eb0b3e4adb927ef99c3f5c9b0e2d55c404a4d3a
ok <swagmaster69@swag.com>
2020-08-15 20:25:14 -0400

    improve classloadcheck code

--- src/main/kotlin/dev/tigr/theia/checks/ClassloadCheck.kt
+++ src/main/kotlin/dev/tigr/theia/checks/ClassloadCheck.kt
@@ -3,73 +3,52 @@ package dev.tigr.theia.checks
 import dev.tigr.theia.Possible
 import dev.tigr.theia.Program
 import dev.tigr.theia.Theia
-import org.objectweb.asm.tree.LdcInsnNode
-import org.objectweb.asm.tree.MethodInsnNode
+import org.objectweb.asm.Opcodes
+import org.objectweb.asm.tree.*
+import java.util.function.Consumer
 
 /**
  * @author GiantNuker
  * 6/9/2020
+ * Updated by Crystallinqq 2 fix nukers easily bypassable resource cache check
+ * 8/15/2020
  */
 object ClassloadCheck : AbstractCheck("ClassloadCheck", "Dynamically loads a class (can bypass other checks)") {
     override fun run(program: Program) {
-        var lastLdc: LdcInsnNode? = null
         for (cn in program.getClassNodes().values) {
             if (Theia.isExcluded(cn.name)) continue
-            for (mn in cn.methods) {
-                for (insn in mn.instructions) {
-                    if (insn is LdcInsnNode) {
-                        lastLdc = insn
-                    } else if (insn is MethodInsnNode) {
-                        if (format(insn) == "java/lang/Class:getDeclaredField:(Ljava/lang/String;)Ljava/lang/reflect/Field;") {
-                            if ((lastLdc as LdcInsnNode).cst is String) {
-                                if (lastLdc.cst == "resourceCache") {
-                                    possibles.add(
-                                        Possible(
-                                            Possible.Severity.WARN,
-                                            "Accessed resource cache (dynamic resource/classloading)",
-                                            cn.name
-                                        )
-                                    )
-                                }
-                            }
-                        } else if (format(insn) == "sun/misc/Unsafe:defineClass:(Ljava/lang/String;[BIILjava/lang/ClassLoader;Ljava/security/ProtectionDomain;)Ljava/lang/Class;") {
-                            possibles.add(
-                                Possible(
-                                    Possible.Severity.ALERT,
-                                    "Dynamically defined class with Unsafe",
-                                    cn.name
-                                )
-                            )
-                        } else if (format(insn).endsWith("defineClass:([BII)Ljava/lang/Class;")) {
-                            var lastCn = cn
-                            while (lastCn.superName != "java/lang/ClassLoader" && lastCn.superName != "java/lang/Object") {
-                                if (program.getClassNodes().get(lastCn.superName) != null) {
-                                    lastCn = program.getClassNodes().get(lastCn.superName)!!
-                                } else {
-                                    break
-                                }
-                            }
-                            if (lastCn.superName == "java/lang/ClassLoader") {
+            cn.methods.forEach(Consumer { methodNode: MethodNode ->
+                methodNode.instructions.forEach(Consumer { ain: AbstractInsnNode ->
+                    if (ain is MethodInsnNode)  {
+                        when(ain.name) {
+                            "defineClass" ->
                                 possibles.add(
                                     Possible(
-                                        Possible.Severity.ALERT,
-                                        "Dynamically defined class",
-                                        cn.name
+                                        Possible.Severity.ALERT, "Dynamically defined class with defineClass.", cn.name
                                     )
                                 )
-                            } else {
+                            "getDeclaredField" ->
                                 possibles.add(
-                                    Possible(
-                                        Possible.Severity.CHECK,
-                                        "Might have dynamically defined class",
-                                        cn.name
+                                    Possible(Possible.Severity.WARN, "Accesses field with Reflect API (May be dynamically loading classes/resources)", cn.name
                                     )
                                 )
-                            }
                         }
                     }
+                })
+                if (cn.superName == "java/lang/ClassLoader") {
+                    possibles.add(
+                        Possible(
+                            Possible.Severity.ALERT, "Dynamically defined class", cn.name
+                        )
+                    )
+                } else {
+                    possibles.add(
+                        Possible(
+                            Possible.Severity.CHECK, "Might have dynamically defined class", cn.name
+                        )
+                    )
                 }
-            }
+            })
         }
     }
 }
